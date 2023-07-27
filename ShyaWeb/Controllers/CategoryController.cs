cbce191295bce5267de16f80a6ff7426ccb11988
cungaminoan <cungaminoan@gmail.com>
2023-07-27 16:44:02 +0700

    Add project files.

---
+++ ShyaWeb/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShyaWeb.Data;
+using ShyaWeb.Models;
+
+namespace ShyaWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
